791ba0aa5694480f23718058e494a9902f6310ae
Les Aker <me@lesaker.org>
2026-07-09 21:38:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+puppet-bulldozer
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/bulldozer.svg)](https://forge.puppetlabs.com/halyard/bulldozer)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/halyard/puppet-bulldozer/build.yml?branch=main)](https://github.com/halyard/puppet-bulldozer/actions)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure [bulldozer](https://github.com/palantir/bulldozer) app
+
+## Usage
+
+```puppet
+include bulldozer
+```
+## License
+
+bulldozer is released under the MIT License. See the bundled LICENSE file for details.
+
---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+# @summary Configure bulldozer container
+#
+# @param datadir sets where to store the config file
+# @param hostname is the hostname
+# @param aws_access_key_id sets the AWS key to use for Route53 challenge
+# @param aws_secret_access_key sets the AWS secret key to use for the Route53 challenge
+# @param email sets the contact address for the certificate
+# @param github_integration_id sets the integration ID for the GitHub app
+# @param github_webhook_secret sets the webook secret for validating webhooks
+# @param github_private_key sets the private key for the GitHub app
+# @param github_client_id sets the Client ID for OAuth
+# @param github_client_secret sets the Client Secret for OAuth
+# @param session_key sets the cookie encryption key
+# @param container_ip sets the IP address for the docker container
+class bulldozer (
+  String $datadir,
+  String $hostname,
+  String $aws_access_key_id,
+  String $aws_secret_access_key,
+  String $email,
+  String $github_integration_id,
+  String $github_webhook_secret,
+  String $github_private_key,
+  String $container_ip = '172.17.0.3',
+) {
+  file { $datadir:
+    ensure => directory,
+  }
+
+  file { "${datadir}/bulldozer.yml":
+    ensure  => file,
+    content => template('bulldozer/bulldozer.yml.erb'),
+    notify  => Service['container@bulldozer'],
+  }
+
+  docker::container { 'bulldozer':
+    image => 'palantirtechnologies/bulldozer:latest',
+    args  => [
+      "-v ${datadir}:/secrets",
+    ],
+    cmd   => '',
+  }
+
+  nginx::site { $hostname:
+    proxy_target          => "http://${container_ip}:8080",
+    aws_access_key_id     => $aws_access_key_id,
+    aws_secret_access_key => $aws_secret_access_key,
+    email                 => $email,
+  }
+}
